a3bf2d30aead20a9afbf3e955374d89312c859d2
sasha <alexandranakonechnaya95@gmail.com>
2018-08-14 10:23:16 +0200

    first commit

---
+++ 10/my.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+
+    $('.slider').slick({
+
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        prevArrow: '<img class="pre" src="img/Layer%2010%20copy.png">',
+        nextArrow: '<img class="next" src="img/Layer%2010.png">'
+
+    });
+
+
+});
+
